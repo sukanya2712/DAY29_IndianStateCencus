704c8e2d99683b25fc7a4e678b4b47164d30a569
sukanya2712 <sukanyanaik511@gmail.com>
2023-04-13 11:27:48 +0530

    added

---
+++ src/main/java/CensusAnalyserException.java
@@ -0,0 +1,17 @@
+public class CensusAnalyserException extends Throwable {
+    enum ExceptionType {
+        CENSUS_FILE_PROBLEM
+    }
+
+    ExceptionType type;
+
+    public CensusAnalyserException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
+
+    public CensusAnalyserException(String message, ExceptionType type, Throwable cause) {
+        super(message, cause);
+        this.type = type;
+    }
+}
---
+++ src/main/java/IndiaCensusCSV.java
@@ -0,0 +1,24 @@
+import com.opencsv.bean.CsvBindByName;
+public class IndiaCensusCSV {
+    @CsvBindByName(column = "SrNo", required = true)
+    public int srNo;
+
+    @CsvBindByName(column = "State Name", required = true)
+    public String stateName;
+
+    @CsvBindByName(column = "TIN", required = true)
+    public int tin;
+
+    @CsvBindByName(column = "StateCode", required = true)
+    public String stateCode;
+
+    @Override
+    public String toString() {
+        return "IndiaCensusCSV{" +
+                "srNo=" + srNo +
+                ", stateName='" + stateName + '\'' +
+                ", tin=" + tin +
+                ", stateCode='" + stateCode + '\'' +
+                '}';
+    }
+}
